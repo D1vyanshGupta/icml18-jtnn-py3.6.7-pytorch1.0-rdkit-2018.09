febbc1b87ef941890c3c955e933a65a2e8de554e
Divyansh Gupta <15144652+D1vyanshGupta@users.noreply.github.com>
2019-01-18 11:28:29 +0800

    testing nn.DataParallel

--- new_molvae/vae_train.py
+++ new_molvae/vae_train.py
@@ -124,7 +124,7 @@ meters = np.zeros(6)
 for epoch in range(args.epoch):
     epoch_time = 0
     print('Jai Mata Di!!')
-    loader = MolTreeFolder(args.train, vocab, args.use_graph_conv, args.batch_size, num_workers=5)
+    loader = MolTreeFolder(args.train, vocab, args.use_graph_conv, args.batch_size, device, num_workers=5)
     for idx, batch in enumerate(loader):
         total_step += 1
         try:
