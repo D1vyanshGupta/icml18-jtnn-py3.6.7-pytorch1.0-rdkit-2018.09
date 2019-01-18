b8788a47c6c39ff13af2cdac93fa9164597e9673
Divyansh Gupta <15144652+D1vyanshGupta@users.noreply.github.com>
2019-01-19 01:08:57 +0800

    fix batch size

--- new_molvae/vae_train.py
+++ new_molvae/vae_train.py
@@ -40,7 +40,7 @@ parser.add_argument('--log_dir', required=True)
 parser.add_argument('--load_epoch', type=int, default=0)
 
 parser.add_argument('--hidden_size', type=int, default=450)
-parser.add_argument('--batch_size', type=int, default=32)
+parser.add_argument('--batch_size', type=int, default=40)
 parser.add_argument('--latent_size', type=int, default=56)
 parser.add_argument('--depthT', type=int, default=20)
 parser.add_argument('--depthG', type=int, default=3)
@@ -120,142 +120,141 @@ for epoch in range(args.epochs):
     epoch_time = 0
     loader = MolTreeFolder(args.train, vocab, args.use_graph_conv, args.batch_size, num_workers=5)
     for idx, batch in enumerate(loader):
-        print('Epoch: {}, Iteration: {}'.format(epoch, idx))
-#         total_step += 1
-#         try:
-#             start = timer()
-#
-#             # reset the gradient buffer to 0.
-#             model.zero_grad()
-#             # implement forward pass
-#             loss, kl_div, wacc, tacc, aacc, sacc = model(batch, beta)
-#
-#             # append items to list
-#             loss_lst.append(loss.item())
-#             kl_div_lst.append(kl_div)
-#             wacc_lst.append(wacc)
-#             tacc_lst.append(tacc)
-#             aacc_lst.append(aacc)
-#             sacc_lst.append(sacc)
-#
-#             # implement backpropagation
-#             loss.backward()
-#
-#             # implement gradient clipping
-#             # nn.utils.clip_grad_norm_(model.parameters(), args.clip_norm)
-#
-#             # update model parameters
-#             optimizer.step()
-#
-#             end = timer()
-#
-#             time_for_iteration = (end - start)
-#
-#             epoch_time += time_for_iteration
-#
-#         except Exception as e:
-#             print(e)
-#             continue
-#
-#         meters = meters + np.array([loss.item(), kl_div, wacc * 100, tacc * 100, aacc * 100, sacc * 100])
-#
-#         if total_step % args.print_iter == 0:
-#             meters /= args.print_iter
-#             print("Epoch:{}, Iteration: {}, Step: {}, Beta: {:.3f}, Loss:{:.2f}, KL: {:.2f}, Word: {:.2f}, Topo: {:.2f}, Assm: {:.2f}, Stereo: {:.2f}, Time: {:.2f} min".format(
-#                     epoch + 1, idx + 1, total_step, beta, meters[0], meters[1], meters[2], meters[3], meters[4], meters[5], time_for_iteration / 60))
-#             sys.stdout.flush()
-#             meters *= 0
-#
-#         # implement learning-rate annealing
-#         if args.enable_lr_anneal and total_step % args.anneal_iter == 0:
-#             scheduler.step()
-#             print("learning rate: %.6f" % scheduler.get_lr()[0])
-#
-#         # implement beta-annealing
-#         if args.enable_beta_anneal and total_step % args.beta_anneal_iter == 0 and total_step >= args.warmup:
-#             beta = min(args.max_beta, beta + args.step_beta)
-#
-#     # save the model after this epoch
-#     model_name = (args.model_name).lower().replace(' ', '_')
-#     torch.save(model.state_dict(), args.save_dir + "/{}.epoch-".format(model_name) + str(epoch + args.load_epoch + 1))
-#
-#     print('Epoch: {} completed. Time taken: {:.2f} min.'.format(epoch + args.load_epoch + 1, epoch_time / 60))
-#     total_training_time += epoch_time
-#
-# print('Total Training Time: {:.2f} min.'.format(total_training_time / 60))
-# # create model info
-# model_info_line_1 = "Hidden Size: {}, Latent Size: {}, DepthG: {}, DepthT: {}".format(args.hidden_size, args.latent_size, args.depthG, args.depthT)
-# model_info_line_2 = "Batch Size: {}, Initial LR: {}, Initial Beta: {}, Max Beta: {}".format(args.batch_size, args.lr, args.beta, args.max_beta)
-#
-# if args.enable_lr_anneal:
-#     model_info_line_3 = "LR Anneal Rate: {}, LR Anneal Iter: {}".format(args.anneal_rate, args.anneal_iter)
-# else:
-#     model_info_line_3 = "LR Annealing Disabled"
-#
-# if args.enable_beta_anneal:
-#     model_info_line_4 = "Beta Annealing Begins at: {}, Step Beta: {}, Beta Anneal Iter: {}".format(args.warmup,   args.step_beta, args.beta_anneal_iter)
-# else:
-#     model_info_line_4 = "Beta Annealing Disabled"
-#
-# model_info = "\n" + model_info_line_1 + "\n" + model_info_line_2 + "\n" + model_info_line_3 + "\n" + model_info_line_4
-#
-# # figure for loss and kl-divergence
-# # plot graphs
-# fig = plt.figure(0, figsize=(15, 10))
-# plt.plot(np.arange(total_step), loss_lst, label='Overall Loss')
-# plt.plot(np.arange(total_step), kl_div_lst, label='KL-Divergence')
-#
-# plt.title(args.model_name + " " + "Losses" + model_info, fontsize=10, pad=15.0)
-#
-# plt.xlabel('Iterations', fontsize=20, labelpad=10)
-# plt.ylabel('Loss', fontsize=20, labelpad=10)
-#
-# plt.tick_params(labelsize=20)
-# plt.legend()
-#
-# plot_name = (args.model_name).lower().replace(' ', '_') + "_loss"
-#
-# fig_path = os.path.join(PARENT_DIR_PATH, 'plots', plot_name + '.png')
-# plt.savefig(fig_path, dpi=200)
-# plt.close(fig)
-#
-# fig = plt.figure(0, figsize=(15, 10))
-# plt.plot(np.arange(total_step), tacc_lst, label='Topological Accuracy')
-# plt.plot(np.arange(total_step), wacc_lst, label='Label Accuracy')
-# plt.plot(np.arange(total_step), sacc_lst, label='Stereo Accuracy')
-# plt.plot(np.arange(total_step), aacc_lst, label='Assembly Accuracy')
-#
-# plt.title(args.model_name + " " + "Accuracies" + model_info, fontsize=10, pad=15.0)
-#
-# plt.xlabel('Iterations', fontsize=20, labelpad=10)
-# plt.ylabel('Accuracy', fontsize=20, labelpad=10)
-#
-# plt.tick_params(labelsize=20)
-# plt.legend()
-#
-# plot_name = (args.model_name).lower().replace(' ', '_') + "_acc"
-#
-# fig_path = os.path.join(args.plot_dir, plot_name + '.png')
-# plt.savefig(fig_path, dpi=200)
-# plt.close(fig)
-#
-# # save various lists
-# model_name = (args.model_name).lower().replace(' ', '_')
-#
-# with open(args.log_dir + '/{}_loss_lst.pkl'.format(model_name), 'wb') as f:
-#     pickle.dump(loss_lst, f, pickle.HIGHEST_PROTOCOL)
-#
-# with open(args.log_dir + '/{}_kl_div_lst.pkl'.format(model_name), 'wb') as f:
-#     pickle.dump(kl_div_lst, f, pickle.HIGHEST_PROTOCOL)
-#
-# with open(args.log_dir + '/{}_wacc_lst.pkl'.format(model_name), 'wb') as f:
-#     pickle.dump(wacc_lst, f, pickle.HIGHEST_PROTOCOL)
-#
-# with open(args.log_dir + '/{}_tacc_lst.pkl'.format(model_name), 'wb') as f:
-#     pickle.dump(tacc_lst, f, pickle.HIGHEST_PROTOCOL)
-#
-# with open(args.log_dir + '/{}_aacc_lst.pkl'.format(model_name), 'wb') as f:
-#     pickle.dump(aacc_lst, f, pickle.HIGHEST_PROTOCOL)
-#
-# with open(args.log_dir + '/{}_sacc_lst.pkl'.format(model_name), 'wb') as f:
-#     pickle.dump(sacc_lst, f, pickle.HIGHEST_PROTOCOL)
+        total_step += 1
+        try:
+            start = timer()
+
+            # reset the gradient buffer to 0.
+            model.zero_grad()
+            # implement forward pass
+            loss, kl_div, wacc, tacc, aacc, sacc = model(batch, beta)
+
+            # append items to list
+            loss_lst.append(loss.item())
+            kl_div_lst.append(kl_div)
+            wacc_lst.append(wacc)
+            tacc_lst.append(tacc)
+            aacc_lst.append(aacc)
+            sacc_lst.append(sacc)
+
+            # implement backpropagation
+            loss.backward()
+
+            # implement gradient clipping
+            # nn.utils.clip_grad_norm_(model.parameters(), args.clip_norm)
+
+            # update model parameters
+            optimizer.step()
+
+            end = timer()
+
+            time_for_iteration = (end - start)
+
+            epoch_time += time_for_iteration
+
+        except Exception as e:
+            print(e)
+            continue
+
+        meters = meters + np.array([loss.item(), kl_div, wacc * 100, tacc * 100, aacc * 100, sacc * 100])
+
+        if total_step % args.print_iter == 0:
+            meters /= args.print_iter
+            print("Epoch:{}, Iteration: {}, Step: {}, Beta: {:.3f}, Loss:{:.2f}, KL: {:.2f}, Word: {:.2f}, Topo: {:.2f}, Assm: {:.2f}, Stereo: {:.2f}, Time: {:.2f} min".format(
+                    epoch + 1, idx + 1, total_step, beta, meters[0], meters[1], meters[2], meters[3], meters[4], meters[5], time_for_iteration / 60))
+            sys.stdout.flush()
+            meters *= 0
+
+        # implement learning-rate annealing
+        if args.enable_lr_anneal and total_step % args.anneal_iter == 0:
+            scheduler.step()
+            print("learning rate: %.6f" % scheduler.get_lr()[0])
+
+        # implement beta-annealing
+        if args.enable_beta_anneal and total_step % args.beta_anneal_iter == 0 and total_step >= args.warmup:
+            beta = min(args.max_beta, beta + args.step_beta)
+
+    # save the model after this epoch
+    model_name = (args.model_name).lower().replace(' ', '_')
+    torch.save(model.state_dict(), args.save_dir + "/{}.epoch-".format(model_name) + str(epoch + args.load_epoch + 1))
+
+    print('Epoch: {} completed. Time taken: {:.2f} min.'.format(epoch + args.load_epoch + 1, epoch_time / 60))
+    total_training_time += epoch_time
+
+print('Total Training Time: {:.2f} min.'.format(total_training_time / 60))
+# create model info
+model_info_line_1 = "Hidden Size: {}, Latent Size: {}, DepthG: {}, DepthT: {}".format(args.hidden_size, args.latent_size, args.depthG, args.depthT)
+model_info_line_2 = "Batch Size: {}, Initial LR: {}, Initial Beta: {}, Max Beta: {}".format(args.batch_size, args.lr, args.beta, args.max_beta)
+
+if args.enable_lr_anneal:
+    model_info_line_3 = "LR Anneal Rate: {}, LR Anneal Iter: {}".format(args.anneal_rate, args.anneal_iter)
+else:
+    model_info_line_3 = "LR Annealing Disabled"
+
+if args.enable_beta_anneal:
+    model_info_line_4 = "Beta Annealing Begins at: {}, Step Beta: {}, Beta Anneal Iter: {}".format(args.warmup,   args.step_beta, args.beta_anneal_iter)
+else:
+    model_info_line_4 = "Beta Annealing Disabled"
+
+model_info = "\n" + model_info_line_1 + "\n" + model_info_line_2 + "\n" + model_info_line_3 + "\n" + model_info_line_4
+
+# figure for loss and kl-divergence
+# plot graphs
+fig = plt.figure(0, figsize=(15, 10))
+plt.plot(np.arange(total_step), loss_lst, label='Overall Loss')
+plt.plot(np.arange(total_step), kl_div_lst, label='KL-Divergence')
+
+plt.title(args.model_name + " " + "Losses" + model_info, fontsize=10, pad=15.0)
+
+plt.xlabel('Iterations', fontsize=20, labelpad=10)
+plt.ylabel('Loss', fontsize=20, labelpad=10)
+
+plt.tick_params(labelsize=20)
+plt.legend()
+
+plot_name = (args.model_name).lower().replace(' ', '_') + "_loss"
+
+fig_path = os.path.join(PARENT_DIR_PATH, 'plots', plot_name + '.png')
+plt.savefig(fig_path, dpi=200)
+plt.close(fig)
+
+fig = plt.figure(0, figsize=(15, 10))
+plt.plot(np.arange(total_step), tacc_lst, label='Topological Accuracy')
+plt.plot(np.arange(total_step), wacc_lst, label='Label Accuracy')
+plt.plot(np.arange(total_step), sacc_lst, label='Stereo Accuracy')
+plt.plot(np.arange(total_step), aacc_lst, label='Assembly Accuracy')
+
+plt.title(args.model_name + " " + "Accuracies" + model_info, fontsize=10, pad=15.0)
+
+plt.xlabel('Iterations', fontsize=20, labelpad=10)
+plt.ylabel('Accuracy', fontsize=20, labelpad=10)
+
+plt.tick_params(labelsize=20)
+plt.legend()
+
+plot_name = (args.model_name).lower().replace(' ', '_') + "_acc"
+
+fig_path = os.path.join(args.plot_dir, plot_name + '.png')
+plt.savefig(fig_path, dpi=200)
+plt.close(fig)
+
+# save various lists
+model_name = (args.model_name).lower().replace(' ', '_')
+
+with open(args.log_dir + '/{}_loss_lst.pkl'.format(model_name), 'wb') as f:
+    pickle.dump(loss_lst, f, pickle.HIGHEST_PROTOCOL)
+
+with open(args.log_dir + '/{}_kl_div_lst.pkl'.format(model_name), 'wb') as f:
+    pickle.dump(kl_div_lst, f, pickle.HIGHEST_PROTOCOL)
+
+with open(args.log_dir + '/{}_wacc_lst.pkl'.format(model_name), 'wb') as f:
+    pickle.dump(wacc_lst, f, pickle.HIGHEST_PROTOCOL)
+
+with open(args.log_dir + '/{}_tacc_lst.pkl'.format(model_name), 'wb') as f:
+    pickle.dump(tacc_lst, f, pickle.HIGHEST_PROTOCOL)
+
+with open(args.log_dir + '/{}_aacc_lst.pkl'.format(model_name), 'wb') as f:
+    pickle.dump(aacc_lst, f, pickle.HIGHEST_PROTOCOL)
+
+with open(args.log_dir + '/{}_sacc_lst.pkl'.format(model_name), 'wb') as f:
+    pickle.dump(sacc_lst, f, pickle.HIGHEST_PROTOCOL)
